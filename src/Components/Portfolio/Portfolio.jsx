fa63ec3f826af1b1c2ad2fd45c07243719e6ec4c
Asaduzzaman Akin <43325109+Akin19@users.noreply.github.com>
2025-09-23 20:24:36 +0000

    Updated portfolio image

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import "./Portfolio.css";
 
-import IMG1 from "../../assets/tobey-Usnic.png";
+import IMG1 from "../../assets/tobeyUsnic.png";
 import IMG2 from "../../assets/portfolio2.jpg";
 import IMG3 from "../../assets/portfolio3.jpg";
 import IMG4 from "../../assets/portfolio4.jpg";
